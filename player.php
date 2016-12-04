1c5a11dc48cb7fb391952d6e66c3830a965d2c62
[ruby] <[s102213066@mail1.ncnu.edu.tw]>
2016-12-04 13:42:47 +0800

    更改排版，介面完整化

--- player.php
+++ player.php
@@ -2,7 +2,6 @@
 session_start();
 require("Msg.php");
 $userName=$_SESSION['uID'];
-$result=get($userName);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -12,35 +11,47 @@ $result=get($userName);
 </head>
 
 <body>
+<p><?php echo $userName;?>您好&nbsp [<a href="logout.php">登出</a>]</p>
 <?php 
 $r = getMoney($userName);
-if ($result) {
+if ($r) {
 	while (	$r=mysqli_fetch_assoc($r)) {
         ?>
-        您現在有<?php echo $r['Money'];?>錢
+        您現在有<?php echo $r['Money'];?>元
     <?php 
         break;
     }
 }
 ?>
-<p><a href="logout.php">登出</a></p>
-<p>您擁有的卡片 </p>
 <hr />
-<!--<a href=getMoney.php> 你有多少錢     </a>-->
+<p>您擁有的卡片 </p>
 <table width="200" border="1">
-  <tr>
-    <td>cardName</td>
-    <td>  </td>
-  </tr>
+<tr><td>cardname</td></tr>
+<?php 
+$result=get($userName);
+if ($result) {
+	while (	$rs=mysqli_fetch_assoc($result)) {
+        ?>
+        <tr>
+        <td><?php echo $rs['cardName'];?></td>
+        <td>賣</td>
+        </tr>
+        <?php 
+	}
+}
+?>
+</table>
 
 <?php
-
+/*
 if ($result) {
 	while (	$rs=mysqli_fetch_assoc($result)) {
 	echo "<tr><td>" . $rs['cardName'] . "</td>";
 	echo "<td> 賣 </td></tr>";
     }
-}
+}*/
 ?>
+<hr />
+<p>您正在競標的卡片</p>
 
 </body>
