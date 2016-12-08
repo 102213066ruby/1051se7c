c9737d2aea3dee4d68b1c65ae4267499dc4fb8eb
candy80200280 <candy80200280@gmail.com>
2016-12-08 20:16:12 +0800

    Add files via upload
    增加拍賣table

--- player.php
+++ player.php
@@ -2,12 +2,10 @@
 session_start();
 require("Msg.php");
 $userName=$_SESSION['uID'];
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>您擁有的卡片</title>
 </head>
 
 <body>
@@ -15,17 +13,19 @@ $userName=$_SESSION['uID'];
 <?php 
 $r = getMoney($userName);
 if ($r) {
-	while (	$r=mysqli_fetch_assoc($r)) {
-        ?>
-        您現在有<?php echo $r['Money'];?>元
-    <?php 
-        break;
-    }
-}
-?>
-<hr />
+ 	while (	$r=mysqli_fetch_assoc($r)) {
+         ?>
+         您現在有<?php echo $r['Money'];?>元
+     <?php 
+         break;
+     }
+ }
+ ?>
+
+ <hr />
+<!--<a href=getMoney.php> 你有多少錢     </a>-->
 <p>您擁有的卡片 </p>
-<table width="200" border="1">
+ <table width="200" border="1">
 <tr><td>cardname</td></tr>
 <?php 
 $result=get($userName);
@@ -41,17 +41,22 @@ if ($result) {
 }
 ?>
 </table>
-
-<?php
-/*
-if ($result) {
-	while (	$rs=mysqli_fetch_assoc($result)) {
-	echo "<tr><td>" . $rs['cardName'] . "</td>";
-	echo "<td> 賣 </td></tr>";
-    }
-}*/
-?>
+ 
+ <?php
+ /*
+ if ($result) {
+ 	while (	$rs=mysqli_fetch_assoc($result)) {
+ 	echo "<tr><td>" . $rs['cardName'] . "</td>";
+ 	echo "<td> 賣 </td></tr>";
+     }
+-}
++}*/
+ ?>
 <hr />
 <p>您正在競標的卡片</p>
-
-</body>
+ 
+ </body>
+ <hr />
+<p>您正在拍賣的卡片</p>
+ 
+ </body>
